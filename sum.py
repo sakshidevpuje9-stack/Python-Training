50cf26c076590213b2ad13650dabdc18ef3fe1e2
sakshi Devpuje <sakshidevpuje@gmal.com>
2026-05-29 21:35:48 +0530

    change in marks grade a percentage

--- sum.py
+++ sum.py
@@ -1,2 +1,18 @@
-
+s1=int (input("Enter the first number: "))
+s2=int (input("Enter the second number: "))
+s3=int (input("Enter the third number: "))
+sum=s1+s2+s3
+print("The sum of the three numbers is: ",sum)
+percentage=(sum/300)*100
+print("The percentage is: ",percentage)
+if percentage>=90:
+    print("Grade: A")
+elif percentage>=80:
+    print("Grade: B")
+elif percentage>=70:
+    print("Grade: C")
+elif percentage>=60:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
