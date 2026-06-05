4e2018cf24c9f3c7c1b0df13e54277660d9d4fc3
sakshi Devpuje <sakshidevpuje@gmal.com>
2026-06-05 12:04:49 +0530

    HTML CODE

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
 
+from pprint import pp
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+Student_data = [
+                 {"name": "Sakshi", "score": 80,"Attempt": 1,"Status": "Pass"},
+                 {"name": "Arti", "score": 90,"Attempt": 2,"Status": "Pass"},
+                {"name": "Satyarth", "score": 70,"Attempt": 1,"Status": "Pass"},
+                {"name": "Shivam", "score": 60,"Attempt": 3,"Status": "Pass"},
+                 
+]
+
+@app.route('/')
+def Home():
+    return render_template('Home.html')
+
+@app.route('/About')
+def About():
+    return render_template('About.html')
+
+@app.route('/Students')
+def Students():
+    return render_template('Stud.html', students=Student_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
